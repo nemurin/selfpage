bc50a9a19aea2ab48d92697eed3cf6cc610bf517
nemurin <yh.kyud.28@gmail.com>
2020-02-08 00:23:41 +0900

    バグ修正

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Output from '@/components/Output'
+import Work from '@/components/Work'
 
 Vue.use(Router)
 
