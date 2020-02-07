a30ec4eff42a9f3afbde357e924a82f086b0a8ea
nemurin <yh.kyud.28@gmail.com>
2020-02-08 00:35:02 +0900

    スキルの追加

---
+++ project/src/components/Skill.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="skill">
+    <div class="content">
+    <h3> {{ name }} </h3>
+    <p> {{ text }} </p>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skill',
+  data () {
+    return {
+      name: 'こういうスキルもってます',
+      text: 'このような意図で作成しました。ここをこのように工夫しました。ここが難しかったです。今後はこうゆう機能を追加したいです。'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+div.skill {
+  text-align: center;
+}
+
+div.content {
+  background-color: #eee;
+  display: inline-block;
+  margin: 10px;
+  width: 30%;
+}
+
+h3 {
+  color: white;
+}
+
+p {
+  width: 90%;
+  display: inline-block;
+  text-align: left;
+}
+
+</style>
--- project/src/router/index.js
+++ project/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Work from '@/components/Work'
+import Skill from '@/components/Skill'
 
 Vue.use(Router)
 
@@ -11,6 +12,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+
+    {
+      path: '/skills',
+      name: 'Skill',
+      component: Skill
     },
 
     {
