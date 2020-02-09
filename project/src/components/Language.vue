ad39524c6e548c04e645dd807a758f714c032de7
nemurin <yh.kyud.28@gmail.com>
2020-02-09 18:43:21 +0900

    Languageのページを充実させた

--- project/src/components/Language.vue
+++ project/src/components/Language.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="language">
-    <div class="content">
-    <h3> {{ name }} </h3>
-    <p> {{ text }} </p>
+    <h1> LANGUAGE </h1>
+    <h3> 使用した経験のある言語をまとめています。３つ星で習熟度を表しています </h3>
+    <div class="content" v-for="(item, index) in items" :key=index>
+    <h2> {{ item.name }} </h2>
+    <h2> {{ item.star }} </h2>
+    <p> {{ item.text }} </p>
   </div>
   </div>
 </template>
@@ -12,8 +15,53 @@ export default {
   name: 'Language',
   data () {
     return {
-      name: 'プロフィール',
-      text: ''
+      items: [
+        {
+      name: 'C++',
+      star: '★★☆',
+      text: '研究で画像処理を行う際にOPENCVとともに使用しています。またDXライブラリとともにゲーム製作にも使用しています。'
+        },
+        {
+      name: 'C#',
+      star: '★★☆',
+      text: 'AtCoderで競技プログラミングをする際に使用しています。またUnityを使用したゲーム製作にも使用しています。'
+        },
+        {
+      name: 'Vue.js',
+      star: '★☆☆',
+      text: 'このサイトを作成するために勉強しました。静的なページしか作れないのでいずれ動的なものを作りたいです。'
+        },
+        {
+      name: 'Kotlin',
+      star: '★☆☆',
+      text: 'LINEのインターンにてサーバーサイドの作業を行うために使用しました。'
+        },
+        {
+      name: 'HTML',
+      star: '★☆☆',
+      text: '最初にホームページを作った際に勉強しました。'
+        },
+        {
+      name: 'Java',
+      star: '★☆☆',
+      text: 'Springフレームワークの勉強に使用しています。'
+        },
+        {
+      name: 'Matlab',
+      star: '★☆☆',
+      text: '研究で最適化処理を行うために使用しています。'
+        },
+        {
+      name: 'Python',
+      star: '★☆☆',
+      text: '授業で機械学習をする際に使用しました。SckitLearnやTensorFlowが使えます。'
+        },
+        {
+      name: 'R',
+      star: '★☆☆',
+      text: '大学の授業で多変量解析を行う際に使用しました。'
+        },
+      ]
     }
   }
 }
@@ -24,6 +72,7 @@ export default {
 
 div.language {
   text-align: center;
+  display: inline-block;
 }
 
 div.content {
@@ -34,7 +83,7 @@ div.content {
 }
 
 h3 {
-  color: white;
+  
 }
 
 p {
