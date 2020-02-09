03336d3585bb001026f1a602a522b57c8c598c1e
nemurin <yh.kyud.28@gmail.com>
2020-02-09 19:44:44 +0900

    スキルを追加

--- project/src/components/Skill.vue
+++ project/src/components/Skill.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="skill">
-    <div class="content">
-    <h3> {{ name }} </h3>
-    <p> {{ text }} </p>
+    <h1> SKILLS </h1>
+    <h3> 使用した経験のあるソフトやスキルをまとめています。３つ星で習熟度を表しています </h3>
+    <div class="content" v-for="(item, index) in items" :key=index>
+    <h2> {{ item.name }} </h2>
+    <h2> {{ item.star }} </h2>
+    <p> {{ item.text }} </p>
   </div>
   </div>
 </template>
@@ -12,8 +15,53 @@ export default {
   name: 'Skill',
   data () {
     return {
-      name: 'こういうスキルもってます',
-      text: 'このような意図で作成しました。ここをこのように工夫しました。ここが難しかったです。今後はこうゆう機能を追加したいです。'
+      items: [
+        {
+      name: 'GitHub',
+      star: '★★★',
+      text: '普段はSourceTreeやTortoiseGit等のクライアントを使用しています。また、プルリクエスト、レビューといった複数人での開発の経験があります。'
+        },
+        {
+      name: 'OpenCV',
+      star: '★★★',
+      text: '研究で画像処理を行う際に使用しています。ホモグラフィー変換、特徴点抽出、画像映像入出力、基本画像処理等の機能について使用経験があります。'
+        },
+        {
+      name: 'Unity',
+      star: '★★★',
+      text: 'ゲーム製作やARNaviプロジェクトに使用しています。複数人開発でのCollaborate機能、ゲームパッド入力対応、Andoroid独自のコンパス等を使用した経験があります。'
+        },
+        {
+      name: 'Firebase',
+      star: '★☆☆',
+      text: 'このサイトのHostingやチャットアプリ製作時の認証機能で使用しています。このサイトにも近々認証やデータベースなどを搭載できればと考えています。'
+        },
+        {
+      name: 'AWS',
+      star: '★☆☆',
+      text: '研究でMatlab計算などの重い処理を行う際に使用しています。EC2インスタンス以外にも様々な機能が存在するので興味を持っています。'
+        },
+        {
+      name: 'Vue.js',
+      star: '★☆☆',
+      text: 'モダンなWeb開発に最近興味があり、勉強を始めています。このサイトをVue.jsで作成することを目標に学習中です。'
+        },
+        {
+      name: 'Blender',
+      star: '★☆☆',
+      text: 'Unityに登場させる3Dモデルの作成を行うために使用しています。'
+        },
+        {
+      name: 'AviUtl',
+      star: '★☆☆',
+      text: '簡単な動画を作る際に使用しています。'
+        },
+        {
+      name: 'CakeWalkBandlab',
+      star: '★☆☆',
+      text: '簡単な効果音を作成する際に使用しています。'
+        },
+      ]
     }
   }
 }
@@ -34,7 +82,6 @@ div.content {
 }
 
 h3 {
-  color: white;
 }
 
 p {
